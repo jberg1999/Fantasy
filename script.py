30e8d9d6bf53f9789cc1654c24405d3dedd4d8aa
jberg1999 <65834469+jberg1999@users.noreply.github.com>
2020-09-25 18:53:58 -0500

    testing out loading ipynb file to website

---
+++ script.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("data/yearly/2010.csv", index_col=0)
+df["Year"] = "2010"
+#df["Id"] = df["Player"] + " " + df["Year"]
+#df.set_index("Id")
+print(df)
+print(df.columns)
+#df["ReceivingTD"].plot.hist()
+#plt.show()
+
+
+path = "data/yearly/2019.csv"
+for year in range(2011,2020):
+    newpath = path.replace("2019", str(year))
+    new_df = pd.read_csv(newpath, index_col=0)
+    new_df["Year"] = str(year)
+    #new_df["Id"] = new_df["Player"] + " " + new_df["Year"]
+    print(new_df)
+    df = df.merge(new_df,how="outer")
+
+df["Id"] = df["Player"] + " " + df["Year"]
+df = df.set_index("Id")
+print(df)
--- test.py
+++
@@ -1,6 +0,0 @@
-#import requests
-#import pandas as pd
-
-print("hello world")
-print("hi")
-print("testing gabe")
