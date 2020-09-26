6cd67c144777d9888d65eacb3764249ace0a12b5
jberg1999 <65834469+jberg1999@users.noreply.github.com>
2020-09-26 14:14:39 -0500

    working hyperlink

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ df = pd.read_csv("data/yearly/2010.csv", index_col=0)
 df["Year"] = "2010"
 #df["Id"] = df["Player"] + " " + df["Year"]
 #df.set_index("Id")
-print(df)
+#print(df)
 print(df.columns)
 #df["ReceivingTD"].plot.hist()
 #plt.show()
@@ -16,7 +16,7 @@ for year in range(2011,2020):
     new_df = pd.read_csv(newpath, index_col=0)
     new_df["Year"] = str(year)
     #new_df["Id"] = new_df["Player"] + " " + new_df["Year"]
-    print(new_df)
+    #print(new_df)
     df = df.merge(new_df,how="outer")
 
 df["Id"] = df["Player"] + " " + df["Year"]
